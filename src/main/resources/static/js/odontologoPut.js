6d25e6c82bbd4a5115e5dd3630a8c2ac4def4a84
jpca92 <jpca92@gmail.com>
2023-04-06 13:12:36 -0500

    vista paciente funcionando 06-04-2023 13:12pm

--- src/main/resources/static/js/odontologoPut.js
+++ src/main/resources/static/js/odontologoPut.js
@@ -2,15 +2,15 @@ window.addEventListener('load', function () {
 
 
     //Buscamos y obtenemos el formulario donde estan
-    //los datos que el usuario pudo haber modificado de la pelicula
+    //los datos que el usuario pudo haber modificado del odontologo
     const formulario = document.querySelector('#update_odontologo_form');
 
     formulario.addEventListener('submit', function (event) {
         let odontologoId = document.querySelector('#odontologo_id').value;
 
-        //creamos un JSON que tendrá los datos de la película
-        //a diferencia de una pelicula nueva en este caso enviamos el id
-        //para poder identificarla y modificarla para no cargarla como nueva
+        //creamos un JSON que tendrá los datos del odontologo
+        //a diferencia de un odontologo nuevo en este caso enviamos el id
+        //para poder identificarlo y modificarlo para no cargarla como nuevo
         const formData = {
             id: document.querySelector('#odontologo_id').value,
             apellido: document.querySelector('#apellido').value,
@@ -19,8 +19,8 @@ window.addEventListener('load', function () {
 
         };
 
-        //invocamos utilizando la función fetch la API peliculas con el método PUT que modificará
-        //la película que enviaremos en formato JSON
+        //invocamos utilizando la función fetch la API con el método PUT que modificará
+        //el odontologo que enviaremos en formato JSON
         const url = '/odontologos';
         const settings = {
             method: 'PUT',
---
+++ src/main/resources/static/js/turnoPut.js
@@ -0,0 +1,59 @@
+window.addEventListener('load', function () {
+
+
+    //Buscamos y obtenemos el formulario donde estan
+    //los datos que el usuario pudo haber modificado del turno
+    const formulario = document.querySelector('#update_turno_form');
+
+    formulario.addEventListener('submit', function (event) {
+        let turnoId = document.querySelector('#turno_id').value;
+
+        //creamos un JSON que tendrá los datos del turno
+        //a diferencia de un turno nuevo en este caso enviamos el id
+        //para poder identificarlo y modificarlo para no cargarla como nuevo
+        const formData = {
+            id: document.querySelector('#turno_id').value,
+            odontologo_id: document.querySelector('#odontologo_id').value,
+            paciente_id: document.querySelector('#paciente_id').value,
+            fecha: document.querySelector('#fecha').value,
+
+        };
+
+        //invocamos utilizando la función fetch la API con el método PUT que modificará
+        //el turno que enviaremos en formato JSON
+        const url = '/turnos';
+        const settings = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        }
+          fetch(url,settings)
+          .then(response => response.json())
+
+    })
+ })
+
+    //Es la funcion que se invoca cuando se hace click sobre el id de un turno del listado
+    //se encarga de llenar el formulario con los datos del turno
+    //que se desea modificar
+    function findBy(id) {
+          const url = '/turnos'+"/"+id;
+          const settings = {
+              method: 'GET'
+          }
+          fetch(url,settings)
+          .then(response => response.json())
+          .then(data => {
+              let turno = data;
+              document.querySelector('#turno_id').value = turno.id;
+              document.querySelector('#odontologo_id').value = turno.odontologo;
+              document.querySelector('#paciente_id').value = turno.paciente_id;
+              document.querySelector('#fecha').value = turno.fecha;
+              //el formulario por default esta oculto y al editar se habilita
+              document.querySelector('#div_turno_updating').style.display = "block";
+          }).catch(error => {
+              alert("Error: " + error);
+          })
+      }
